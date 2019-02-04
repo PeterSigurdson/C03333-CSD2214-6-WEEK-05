2be4b770e930c2a2e06caec17aba5a972e1ad097
peter sigurdson <peter@petersigurdson.net>
2019-02-04 16:58:51 -0500

    final commit

--- C03333/js/main.js
+++ C03333/js/main.js
@@ -1,3 +1,4 @@
-﻿var date = new Date();
-document.body.innerHTML = "<h1>the date is " + date + " </h1>";
-
+﻿function peanut() {
+    var date = new Date();
+    document.body.innerHTML = "<h1>the date is " + date + " </h1>";
+}
